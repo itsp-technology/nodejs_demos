70ee53d130c7260bb94a8ade8f017639552b2529
itsp-technology <vivek1996.cloud@gmail.com>
2024-04-15 13:57:06 +0530

    send data from js file to ejs file

--- inbuild/src/Main.js
+++ inbuild/src/Main.js
@@ -15,6 +15,15 @@ app.use(morgan('common',{stream:fs.createWriteStream('./app.log')}));
 let ProMod = require('./controller/producte');
 let CatMod = require("./controller/category");
 
+//app.use(express.static(__dirname+'./public'))
+
+app.set('views','./views');
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index',{t:'this text data from js file to ejs '});
+})
+
 app.use('/products',ProMod);
 app.use('/category',CatMod);
 
